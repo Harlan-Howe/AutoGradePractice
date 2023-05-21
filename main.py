9a84e3ce4bab1d12feb30c54a1ac234bb714d4f2
harlanhowe <harlan.howe@kinkaid.org>
2023-05-20 21:14:00 -0500

    Created simple class.

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class AGTester:
+
+    def __init__(self, name):
+        self.myName = name
+
+    def __repr__(self):
+        return f"Autotest with name {self.myName}"
+
+    def __str__(self):
+        return self.__repr__()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tester = AGTester("Harlan")
+    print(tester)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
